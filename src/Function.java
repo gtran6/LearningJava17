514c9d692131acc1ba582c5ec535855546d9f210
gtran6 <gtran6@depaul.edu>
2022-09-19 13:25:02 -0500

    first commit

--- src/Function.java
+++ src/Function.java
@@ -3,6 +3,14 @@ import jdk.jshell.Snippet;
 import java.util.Scanner;
 
 public class Function {
+
+    public static double calculateTotalMealPrice(double listedPrice, double tipRate, double taxRate) {
+        double tip = tipRate * listedPrice;
+        double tax = taxRate * listedPrice;
+        double result = listedPrice + tip + tax;
+        return result;
+        //System.out.println("total meal price is: " + result);
+    }
     public static void announceTeaTime() {
         System.out.println("waiting for tea time...");
         System.out.println("type in random word and press Enter");
@@ -11,6 +19,9 @@ public class Function {
         System.out.println("it's tea time");
     }
     public static void main(String[] args) {
-        announceTeaTime();
+        //announceTeaTime();
+        //calculateTotalMealPrice( 15.5, 0.2, 0.15 );
+        double totalCost = calculateTotalMealPrice( 15.5, 0.2, 0.15 );
+        System.out.println(totalCost);
     }
 }
