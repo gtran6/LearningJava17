902aacbc76d1d02c8ed245e0ab3995a7f302f8a2
gtran6 <gtran6@depaul.edu>
2022-09-19 12:58:27 -0500

    first commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Best view comes after the hardest climb");
+        String question = "What is the largest planet in the solar system?";
+        String choiceOne = "earth";
+        String choiceTwo = "jupiter";
+        String choiceThree = "saturn";
+        String correctAnswer = choiceTwo;
+
+        System.out.println(question);
+        System.out.println("choose: " + choiceOne + ", " + choiceTwo + ", " + choiceThree);
+        Scanner sc = new Scanner( System.in );
+        String input = sc.next();
+
+        if (correctAnswer.equals( input.toLowerCase() )) {
+            System.out.println("congratz");
+        } else {
+            System.out.println("incorrect");
+        }
     }
 }
